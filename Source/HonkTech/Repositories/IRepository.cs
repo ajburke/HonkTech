ced626fbf08b47c6842b30eb29f0c7bb3e24c168
ajburke <alexander.burke@taxware.com>
2014-05-16 18:23:54 -0400

    Added IRepository interface and Repositories folder

---
+++ Source/HonkTech/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonkTech.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+        void GetAll();
+        void Update(T entity);
+        void Delete(T entity);
+        T GetByID(int id);
+    }
+}
